d9c7d55aa35c9ae6f8f2fcb0d09b63e1034d4aec
fumiyanokesinn <fumiya.shindori@light-inc.com>
2026-01-12 22:29:40 +0900

    ちょっと修正

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,15 +1,40 @@
 return {
+  -- =====================================
+  -- カラースキーム
+  -- =====================================
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = false,
+        terminal_colors = true,
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+
+  -- =====================================
+  -- Treesitter（構文解析）
+  -- =====================================
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "go", "gomod", "gosum", "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "json" },
+        ensure_installed = { "go", "gomod", "gosum", "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "json", "bash", "python" },
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = { enable = true },
       })
     end,
   },
+
+  -- =====================================
+  -- UI / Dashboard
+  -- =====================================
   {
     "folke/snacks.nvim",
     priority = 1000,
@@ -25,7 +50,16 @@ return {
           { section = "startup" },
         },
       },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          files = {
+            hidden = true,
+          },
+        },
+      },
+      indent = { enabled = true },
+      dim = { enabled = true },
     },
     keys = {
       -- ファイル検索
@@ -33,11 +67,154 @@ return {
       { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
       { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
       { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent files" },
+      -- Projects
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
       -- Git
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git status" },
       { "<leader>gc", function() Snacks.picker.git_log() end, desc = "Git commits" },
       { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git branches" },
       { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git file history" },
     },
+  },
+
+  -- =====================================
+  -- LSP設定
+  -- =====================================
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls", "typescript-language-server" },
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Lua
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      -- Go
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      -- TypeScript
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+    end,
+  },
+
+  -- =====================================
+  -- 補完
+  -- =====================================
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
+  -- =====================================
+  -- ステータスライン
+  -- =====================================
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = true,
+          theme = "tokyonight",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+      })
+    end,
+  },
+
+  -- =====================================
+  -- ファイルツリー
+  -- =====================================
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = { "NvimTreeToggle", "NvimTreeOpen" },
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+    },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+        },
+      })
+    end,
+  },
+
+  -- =====================================
+  -- Git統合
+  -- =====================================
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
+      { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff" },
+    },
+  },
+
+  -- =====================================
+  -- アイコン
+  -- =====================================
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
   },
 }
